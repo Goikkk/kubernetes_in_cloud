6d4d700bdb763430a75f2a4fc13b24b44a7cd0a0
Rafał Karwowski <rafalmkarwowski@gmail.com>
2025-11-29 08:28:21 +0100

    clean-up repo

--- Azure/providers.tf
+++ Azure/providers.tf
@@ -17,7 +17,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "9eb5e723-105f-4b88-a99e-d3bdd66f3f4d"
+  subscription_id = var.subscription_id
 }
 
 provider "helm" {
--- Azure/variables.tf
+++ Azure/variables.tf
@@ -4,6 +4,11 @@ variable "project_name" {
   type        = string
 }
 
+variable "subscription_id" {
+  description = "Azure subscription id"
+  type        = string
+}
+
 variable "azure_region" {
   default = "eastus2"
   type    = string
